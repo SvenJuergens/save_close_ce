09bf8436344cee9f948a7d91803a4d15319b51fc
SvenJuergens <SvenJuergens@users.noreply.github.com>
2023-01-26 16:26:29 +0100

    [TASK] bring back compatibility with TYPO3 11

--- ext_tables.php
+++ ext_tables.php
@@ -1,4 +1,7 @@
 <?php
-if (!defined('TYPO3')) {
-    die('Access denied.');
+defined('TYPO3') or die();
+
+if (version_compare(\TYPO3\CMS\Core\Utility\VersionNumberUtility::getNumericTypo3Version(), '12.0.0', '<=')) {
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['Backend\Template\Components\ButtonBar']['getButtonsHook'][]
+        = 'Goran\SaveCloseCe\Hooks\SaveCloseHook->addSaveCloseButton';
 }

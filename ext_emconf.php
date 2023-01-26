7726bd873a8072a3d8c7ef060ed05035537536fc
SvenJuergens <SvenJuergens@users.noreply.github.com>
2023-01-26 16:27:12 +0100

    [TASK] Version 1.3

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => false,
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '1.2.0',
+    'version' => '1.3.0',
     'constraints' =>
         [
             'depends' =>
